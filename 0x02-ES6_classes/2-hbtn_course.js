bb5684d7e55ba850b90410a112f25ca094c2e3b7
eddie9110 <edwinmaregwa@gmail.com>
2024-03-09 09:33:41 +0000

    task 2 added

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,40 @@
+export default class HolbertonCourse {
+    constructor (name, length, students) {
+      this.name = name;
+      this.length = length;
+      this.students = students;
+    }
+  
+    set name(newName) {
+      if (typeof newName !== "string") {
+        throw TypeError('Name must be a string');
+      }
+      this._name = newName;
+    }
+  
+    get name() {
+      return this._name;
+    }
+  
+    set length(newLength) {
+      if (typeof newLength !== "number") {
+        throw TypeError('Length must be a number');
+      }
+      this._length = newLength;
+    }
+  
+    get length() {
+      return this._length;
+    }
+  
+    set students(newStudents) {
+      if (!Array.isArray(newStudents) || newStudents.every((student) => typeof student !== 'string')) {
+        throw TypeError('Students must be an array of strings');
+      }
+      this._students = newStudents;
+    }
+
+    get students() {
+      return this._students;
+    }
+}
