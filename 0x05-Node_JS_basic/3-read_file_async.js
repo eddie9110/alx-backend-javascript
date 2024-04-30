4e8f74dd23df4bf358c6fd8e28db741ba4fea46d
eddie9110 <edwinmaregwa@gmail.com>
2024-04-30 10:47:48 +0000

    task 3 & 5 added

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(Error('Cannot load the database'));
+        return;
+      }
+      const fileContents = data.toString().split('\n');
+      let students = fileContents.filter((item) => item);
+      students = students.map((item) => item.split(','));
+
+      const fields = {};
+      for (const i in students) {
+        if (i !== 0) {
+          if (!fields[students[i][3]]) {
+            fields[students[i][3]] = [];
+          }
+          fields[students[i][3]].push(students[i][0]);
+        }
+      }
+
+      delete fields.field;
+      let info;
+      const numOfStudents = students.length ? students.length - 1 : 0;
+      info = `Number of students: ${numOfStudents}`;
+      const output = '';
+      output.push(info);
+
+      for (const key of Object.keys(fields)) {
+        info = `Number of students in ${key}: ${
+          fields[key].length
+        }. List: ${fields[key].join(', ')}`;
+
+        console.log(info);
+        output.push(info);
+      }
+      resolve(output);
+    });
+  });
+}
+
+module.exports = countStudents;
---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const { url } = req;
+  if (url === '/') {
+    res.write('Hello Holberton School!');
+  } else if (url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      const students = countStudents(process.argv[2]);
+      res.end(`${students.join('\n')}`);
+    } catch (error) {
+      res.end(error.message);
+    }
+  }
+  res.end();
+});
+
+app.listen(1245, () => {});
+
+module.exports = app;
