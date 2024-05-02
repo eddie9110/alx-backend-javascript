a921cab0def408551e5712b2c8a8d781301699fe
eddie9110 <edwinmaregwa@gmail.com>
2024-05-02 16:56:06 +0000

    formatting

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -4,22 +4,22 @@ const calculateNumber = require("./2-calcul_chai");
 
 describe("calculateNumber", () => {
   describe("SUM", function() {
-	  it("checking rounded sum of ints", () => {
-	    chai.expect(calculateNumber("SUM", 1, 5)).to.equal(6);
-	  });
-	  it("Checking rounded float sum", () => {
-	    chai.expect(calculateNumber("SUM", 1.3, 2.1)).to.equal(3);
-	  });
+    it("checking rounded sum of ints", () => {
+      chai.expect(calculateNumber("SUM", 1, 5)).to.equal(6);
+    });
+    it("Checking rounded float sum", () => {
+      chai.expect(calculateNumber("SUM", 1.3, 2.1)).to.equal(3);
+    });
   });
 
   describe("SUBTRACT", () => {
-	  it("checking if numbers round", () => {
-	    chai.expect(calculateNumber("SUBTRACT", 4.7, 2.8)).to.equal(2);
-	  });
+    it("checking if numbers round", () => {
+      chai.expect(calculateNumber("SUBTRACT", 4.7, 2.8)).to.equal(2);
+    });
 
     it("checking if numbers round", () => {
-	    chai.expect(calculateNumber("SUBTRACT", -4.7, -2.8)).to.equal(-2);
-	  });
+      chai.expect(calculateNumber("SUBTRACT", -4.7, -2.8)).to.equal(-2);
+    });
   });
 
   describe("DIVIDE", () => {
