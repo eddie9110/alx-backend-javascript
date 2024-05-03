02ed577bbeedd3949a319b630463960ca6e4ad68
eddie9110 <edwinmaregwa@gmail.com>
2024-05-03 08:14:42 +0000

    done

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -7,7 +7,7 @@ describe('sendPaymentRequestToApi', () => {
   it('should call calculateNumber', () => {
     const spyCalcNum = sinon.spy(Utils, 'calculateNumber');
     sendPaymentRequestToApi(100, 20);
-    assert(spyCalcNum.withArgs("The total is: 10").calledOnce);
+    assert(spyCalcNum.calledOnce);
     assert(spyCalcNum.withArgs("SUM", 100, 20).calledOnce);
     spyCalcNum.restore();
   });
