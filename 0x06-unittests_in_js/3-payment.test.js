6286e6da177c6fdaad662f40efc84ca8962e080b
eddie9110 <edwinmaregwa@gmail.com>
2024-05-03 08:08:43 +0000

    Bug: missing semi-colons added

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,11 +1,11 @@
 const assert = require('assert');
-const Utils = require('./utils')
+const Utils = require('./utils');
 const sinon = require('sinon');
 const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
   it('should call calculateNumber', () => {
-    const spyCalcNum = sinon.spy(Utils, 'calculateNumber')
+    const spyCalcNum = sinon.spy(Utils, 'calculateNumber');
     sendPaymentRequestToApi(100, 20);
     assert(spyCalcNum.withArgs("The total is: 10").calledOnce);
     assert(spyCalcNum.withArgs("SUM", 100, 20).calledOnce);
